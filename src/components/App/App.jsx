bfdc1460365dde2b39faa3bf04c12823e2eaf6e7
Aldeir Francisco <dide123francisco@gmail.com>
2020-09-10 20:24:32 -0300

    add app components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Wrapper} from './App.styles'
+function App () {
+    return <Wrapper>
+        <Container>
+
+       top. app
+        </Container>
+    </ Wrapper>
+}
+export default App;
--- src/views/Home.views.jsx
+++ src/views/Home.views.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import App from '../components/App'
 function HomeView () {
     return <div>
-        todo: home view.
+     <App />
     </div>
 }
 export default HomeView;
