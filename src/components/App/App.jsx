b6ee1c3f8340cbdd277bee3dc11acf9ce1ecc61e
Aldeir Francisco <dide123francisco@gmail.com>
2020-09-10 22:59:19 -0300

    create porcentagens

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Checkbox from '../../shared/Checkbox/Checkbox';
+import LineChart from '../../shared/LineChart';
 import AppContainer from '../AppContainer';
 import AppHeader from '../AppHeader';
 import { Container, Wrapper} from './App.styles';
@@ -7,6 +8,11 @@ import { Container, Wrapper} from './App.styles';
 function App () {
     const [lettuce, setLettuce] = useState();
     const [ rice, setRice] = useState();
+  
+    const colors = ['#62CBC6','#00ABAD','#00858C','#006073','#004D61']
+
+   
+   
     return <Wrapper>
        
         <Container>
@@ -34,6 +40,10 @@ function App () {
                 }
             right = {<div >
                  estatisticas
+                 <LineChart color = {colors[0]} title = "saudavel" percentage = {80}/>
+                  <LineChart color ={colors[1]} title = "nao tao saudavel assim" percentage = {20}/>
+                  <LineChart color ={colors[2]} title = "limpeza"  percentage = {35} />
+                  <LineChart color ={colors[3]} title = "outros" percentage = {15}/>
                     </div>
                     }
           />
---
+++ src/shared/LineChart/LineChart.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {ProgressBar, Wrapper} from './LineChart.styles'
+
+function LineChart ({title, percentage, color}) {
+     return <Wrapper>
+         <span>
+          {title}:
+         </span>
+         
+         <ProgressBar
+          color={color}
+          percentage ={percentage}
+         />
+     </Wrapper>
+}
+
+export default LineChart;
